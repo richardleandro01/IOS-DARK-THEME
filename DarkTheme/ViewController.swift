998d8d62ad9d2673392f7e2be17cef5157776145
richard <richardp.leandro@gmail.com>
2019-09-28 22:59:05 -0300

    fist

--- DarkTheme/ViewController.swift
+++ DarkTheme/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outletswitch: UISwitch!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func darkAction(_ sender: Any) {
+        if outletswitch.isOn == true{
+            view.backgroundColor = UIColor.black
+            label.textColor = UIColor.white
+        }else{
+            view.backgroundColor = UIColor.white
+            label.textColor = UIColor.black
+        }
+        
+    }
+    
 }
 
